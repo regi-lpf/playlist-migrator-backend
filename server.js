1bb70fe16ec88c91c3930d13f01df215e8955c0a
Reginaldo Leandro Plácido Filho <101012911+regi-lpf@users.noreply.github.com>
2025-05-04 03:29:52 +0000

    Test 2

--- server.js
+++ server.js
@@ -155,8 +155,8 @@ async function getSpotifyToken(clientId, clientSecret) {
   
       res.json({ youtubePlaylistUrl: `https://www.youtube.com/playlist?list=${youtubeId}` });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({status: error.status, error: error.errors.message});
+      console.log(error);
+      res.status(500).json({status: error.status, error: error.errors[0].message});
     }
   });
   
